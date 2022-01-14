dc42225f948cd0e9e572765a89ac4ed3f02425e0
rajesh <rajesh.nitc@gmail.com>
2022-01-15 00:43:37 +0530

    added zsh plugin for terraform

--- 2-setup-zsh.sh
+++ 2-setup-zsh.sh
@@ -14,7 +14,7 @@ echo "export PATH=$PATH:$CLOUDSDK_INSTALL_DIR/google-cloud-sdk/bin:$CLOUDSDK_INS
 echo "export TF_PLUGIN_CACHE_DIR=$HOME/.terraform.d/plugin-cache" >> "$HOME/.zshrc"
 
 sed -i "s/robbyrussell/agnoster/" $HOME/.zshrc
-sed -i "s/plugins=(git)/plugins=(git zsh-autosuggestions web-search gcloud)/" $HOME/.zshrc
+sed -i "s/plugins=(git)/plugins=(git zsh-autosuggestions gcloud terraform)/" $HOME/.zshrc
 echo 'HYPHEN_INSENSITIVE="true"' >> "$HOME/.zshrc"
 echo 'DISABLE_AUTO_TITLE="true"' >> "$HOME/.zshrc"
 echo "CLOUDSDK_HOME=$HOME/.local/google-cloud-sdk" >> "$HOME/.zshrc"
