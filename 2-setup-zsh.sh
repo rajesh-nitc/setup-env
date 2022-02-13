829d3d021471935b57982fd65aef87c61133514a
rajesh-nitc <rajesh.nitc@gmail.com>
2022-02-13 23:40:47 +0530

    Update 2-setup-zsh.sh

--- 2-setup-zsh.sh
+++ 2-setup-zsh.sh
@@ -12,7 +12,6 @@ echo "export PATH=$PATH:$HOME/.local/bin" >> "$HOME/.zshrc"
 # ohmyzsh
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 sed -i "s/robbyrussell/agnoster/" $HOME/.zshrc
-sed -i "s/plugins=(git)/plugins=(git zsh-autosuggestions gcloud terraform)/" $HOME/.zshrc
+sed -i "s/plugins=(git)/plugins=(git zsh-autosuggestions terraform)/" $HOME/.zshrc
 echo 'HYPHEN_INSENSITIVE="true"' >> "$HOME/.zshrc"
 echo 'DISABLE_AUTO_TITLE="true"' >> "$HOME/.zshrc"
-echo "CLOUDSDK_HOME=$HOME/.local/google-cloud-sdk" >> "$HOME/.zshrc"
