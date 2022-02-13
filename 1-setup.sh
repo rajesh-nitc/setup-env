7e9d47be948d4f193f567fd2f7f61885a03158f2
rajesh <rajesh.nitc@gmail.com>
2022-02-13 21:50:24 +0530

    chore

--- 1-setup.sh
+++ 1-setup.sh
@@ -2,37 +2,28 @@
 
 set -xe
 
-CLOUDSDK_INSTALL_DIR=$HOME/.local
 TERRAFORM_VERSION=1.1.5
 TERRAFORM_VALIDATOR_VERSION=2021-03-22
 TERRAFORM_DOCS_VERSION=0.10.1
 
 sudo apt-get update --allow-releaseinfo-change
 sudo apt-get install -y curl wget git unzip
-mkdir -p $CLOUDSDK_INSTALL_DIR/bin
 
 # vs code
 # if wsl: commented
 # if chromebook: uncomment
-# wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-# sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
-# sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
-# sudo apt-get update --allow-releaseinfo-change
-# sudo apt-get install -y code
-# rm packages.microsoft.gpg
-
-# gcloud sdk needs python
-sudo apt install -y python3 python3-dev python3-venv python3-pip 
-pip3 install pip-tools
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
+sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+sudo apt-get update --allow-releaseinfo-change
+sudo apt-get install -y code
+rm packages.microsoft.gpg
 
 # gcloud sdk
-curl https://sdk.cloud.google.com > install_sdk.sh
-chmod +x install_sdk.sh
-export CLOUDSDK_CORE_DISABLE_PROMPTS=1
-./install_sdk.sh --install-dir=$CLOUDSDK_INSTALL_DIR
-$CLOUDSDK_INSTALL_DIR/google-cloud-sdk/bin/gcloud components install skaffold kubectl kustomize kpt nomos
-echo "export PATH=$PATH:$CLOUDSDK_INSTALL_DIR/google-cloud-sdk/bin" >> "$HOME/.bashrc" 
-rm install_sdk.sh
+sudo apt install -y apt-transport-https ca-certificates gnupg
+echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
+sudo apt-get update --allow-releaseinfo-change && sudo apt-get install -y google-cloud-sdk
 
 # terraform
 sudo apt-get install -y unzip
@@ -46,6 +37,10 @@ curl -k -LO https://github.com/terraform-docs/terraform-docs/releases/download/v
 mv terraform-docs-v${TERRAFORM_DOCS_VERSION}-linux-amd64 terraform-docs
 chmod +x terraform-docs
 sudo mv terraform-docs /usr/local/bin
+
+# python
+sudo apt install -y python3 python3-dev python3-venv python3-pip 
+pip3 install pip-tools
 
 # jupyter notebook
 # pip3 install jupyterlab
@@ -62,7 +57,7 @@ pip3 install checkov
 curl https://raw.githubusercontent.com/terraform-linters/tflint/master/install_linux.sh | bash
 
 # terraform-validator
-$CLOUDSDK_INSTALL_DIR/google-cloud-sdk/bin/gsutil cp gs://terraform-validator/releases/${TERRAFORM_VALIDATOR_VERSION}/terraform-validator-linux-amd64 .
+gsutil cp gs://terraform-validator/releases/${TERRAFORM_VALIDATOR_VERSION}/terraform-validator-linux-amd64 .
 chmod +x terraform-validator-linux-amd64
 sudo mv terraform-validator-linux-amd64 /usr/local/bin/terraform-validator
 
--- 2-setup-zsh.sh
+++ 2-setup-zsh.sh
@@ -2,17 +2,12 @@
 
 set -xe
 
-CLOUDSDK_INSTALL_DIR=$HOME/.local
-
-mkdir -p "$HOME/.terraform.d/plugin-cache"
-
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-
-echo "export PATH=$PATH:$CLOUDSDK_INSTALL_DIR/google-cloud-sdk/bin:$CLOUDSDK_INSTALL_DIR/bin" >> "$HOME/.zshrc"
-
 # Terraform Plugin Cache
+mkdir -p "$HOME/.terraform.d/plugin-cache"
 echo "export TF_PLUGIN_CACHE_DIR=$HOME/.terraform.d/plugin-cache" >> "$HOME/.zshrc"
 
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 sed -i "s/robbyrussell/agnoster/" $HOME/.zshrc
 sed -i "s/plugins=(git)/plugins=(git zsh-autosuggestions gcloud terraform)/" $HOME/.zshrc
 echo 'HYPHEN_INSENSITIVE="true"' >> "$HOME/.zshrc"
