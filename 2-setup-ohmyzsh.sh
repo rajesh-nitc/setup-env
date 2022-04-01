8ea1d3e719780eaf53eeacb077cf205b0a14455a
rajesh-nitc <rajesh.nitc@gmail.com>
2022-04-01 12:12:11 +0530

    Update 2-setup-ohmyzsh.sh

--- 2-setup-ohmyzsh.sh
+++ 2-setup-ohmyzsh.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-set -xe
+# set -xe
 
 # Terraform Plugin Cache
-mkdir -p "$HOME/.terraform.d/plugin-cache"
-echo "export TF_PLUGIN_CACHE_DIR=$HOME/.terraform.d/plugin-cache" >> "$HOME/.zshrc"
+# mkdir -p "$HOME/.terraform.d/plugin-cache"
+# echo "export TF_PLUGIN_CACHE_DIR=$HOME/.terraform.d/plugin-cache" >> "$HOME/.zshrc"
 
 # Path
-echo "export PATH=$PATH:$HOME/.local/bin" >> "$HOME/.zshrc"
+# echo "export PATH=$PATH:$HOME/.local/bin" >> "$HOME/.zshrc"
 
 # ohmyzsh
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-sed -i "s/robbyrussell/agnoster/" $HOME/.zshrc
-sed -i "s/plugins=(git)/plugins=(git zsh-autosuggestions terraform)/" $HOME/.zshrc
-echo 'HYPHEN_INSENSITIVE="true"' >> "$HOME/.zshrc"
-echo 'DISABLE_AUTO_TITLE="true"' >> "$HOME/.zshrc"
+# git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+# sed -i "s/robbyrussell/agnoster/" $HOME/.zshrc
+# sed -i "s/plugins=(git)/plugins=(git zsh-autosuggestions terraform)/" $HOME/.zshrc
+# echo 'HYPHEN_INSENSITIVE="true"' >> "$HOME/.zshrc"
+# echo 'DISABLE_AUTO_TITLE="true"' >> "$HOME/.zshrc"
