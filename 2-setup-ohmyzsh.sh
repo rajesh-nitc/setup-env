93d22fa4623d0f40306a60e3899e3b52289b60b3
rajesh-nitc <rajesh.nitc@gmail.com>
2022-04-06 11:49:48 +0530

    Update 2-setup-ohmyzsh.sh

--- 2-setup-ohmyzsh.sh
+++ 2-setup-ohmyzsh.sh
@@ -11,10 +11,12 @@ echo "export PATH=$HOME/.local/bin:$PATH" >> "$HOME/.zshrc"
 
 # ohmyzsh
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-sed -i "s/robbyrussell/af-magic/" $HOME/.zshrc
+sed -i "s/robbyrussell/agnoster/" $HOME/.zshrc
 sed -i "s/plugins=(git)/plugins=(git zsh-autosuggestions terraform)/" $HOME/.zshrc
 echo 'HYPHEN_INSENSITIVE="true"' >> "$HOME/.zshrc"
 echo 'DISABLE_AUTO_TITLE="true"' >> "$HOME/.zshrc"
 
-# MANUALLY PUT QUOTES IN PATH in ~/.zshrc 
-# For e.g. "Program Files" AND "Microsoft VS Code"
+# if wsl
+# update path in ~/.zshrc: For e.g. put quotes around "Program Files" AND "Microsoft VS Code"
+# https://gist.github.com/stramel/658d702f3af8a86a6fe8b588720e0e23
+# update font in wsl debian terminal properties
