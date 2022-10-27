e69a8cd780e88d4510e3bd7d087111dca25d75e9
rajesh-nitc <rajesh.nitc@gmail.com>
2022-10-27 16:04:49 +0530

    Update 2-setup-ohmyzsh.sh

--- 2-setup-ohmyzsh.sh
+++ 2-setup-ohmyzsh.sh
@@ -11,16 +11,37 @@ echo "export PATH=$HOME/.local/bin:$PATH" >> "$HOME/.zshrc"
 
 # ohmyzsh
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-sed -i "s/robbyrussell/macovsky-ruby/" $HOME/.zshrc
+sed -i "s/robbyrussell/agnoster/" $HOME/.zshrc
 sed -i "s/plugins=(git)/plugins=(git zsh-autosuggestions terraform)/" $HOME/.zshrc
 echo 'HYPHEN_INSENSITIVE="true"' >> "$HOME/.zshrc"
 echo 'DISABLE_AUTO_TITLE="true"' >> "$HOME/.zshrc"
 
-# if wsl
-# update path in ~/.zshrc: For e.g. put quotes around "Program Files" AND "Microsoft VS Code"
+# update path (for code. to work) in ~/.zshrc: For e.g. put quotes around "Program Files" AND "Microsoft VS Code"
 # https://gist.github.com/stramel/658d702f3af8a86a6fe8b588720e0e23
 # update font to DejaVu Sans Mono for Powerline in wsl terminal properties
-# optional: install windows terminal
-# set defaults for e.g. color theme: one half dark, font face: inconsolata for powerline, starting dir: /home/rajesh, cursor: vintage
-# startup values for e.g. default profile: debian, launch mode: maximized
-# interaction values for e.g. automatically copy selection to clipboard: On
+
+# vscode dark+ theme on windows terminal
+# {
+#             "background": "#1E1E1E",
+#             "black": "#000000",
+#             "blue": "#2472C8",
+#             "brightBlack": "#666666",
+#             "brightBlue": "#3B8EEA",
+#             "brightCyan": "#29B8DB",
+#             "brightGreen": "#23D18B",
+#             "brightPurple": "#D670D6",
+#             "brightRed": "#F14C4C",
+#             "brightWhite": "#E5E5E5",
+#             "brightYellow": "#F5F543",
+#             "cursorColor": "#808080",
+#             "cyan": "#11A8CD",
+#             "foreground": "#CCCCCC",
+#             "green": "#0DBC79",
+#             "name": "Dark+",
+#             "purple": "#BC3FBC",
+#             "red": "#CD3131",
+#             "selectionBackground": "#FFFFFF",
+#             "white": "#E5E5E5",
+#             "yellow": "#E5E510"
+#         }
+
