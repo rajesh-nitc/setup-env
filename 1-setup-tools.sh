5a24610b4c31ba6755453444c10be57aa920044d
rajesh-nitc <rajesh.nitc@gmail.com>
2023-07-29 19:34:29 +0530

    Update 1-setup-tools.sh

--- 1-setup-tools.sh
+++ 1-setup-tools.sh
@@ -4,17 +4,33 @@
 
 set -xe
 
+PYTHON_VERSION=3.12
 TERRAFORM_VERSION=1.3.1
-PYTHON_VERSION=3.10.0
 
 sudo apt update
 sudo apt install -y curl wget git unzip
 
+# python
+sudo apt install -y software-properties-common
+sudo add-apt-repository ppa:deadsnakes/ppa -y
+sudo apt update
+sudo apt install -y python${PYTHON_VERSION}
+python${PYTHON_VERSION} --version
+sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python${PYTHON_VERSION} 1
+python3 --version
+sudo apt install -y python${PYTHON_VERSION}-dev python${PYTHON_VERSION}-venv
+
+# pip
+wget https://bootstrap.pypa.io/get-pip.py
+sudo python3 get-pip.py
+pip3 --version
+
 # gcloud cli
-sudo apt install -y apt-transport-https ca-certificates gnupg
+sudo apt-get install -y apt-transport-https ca-certificates gnupg curl sudo
 echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
-sudo apt update && sudo apt install -y google-cloud-sdk
+sudo apt-get update
+sudo apt-get install -y google-cloud-cli
 
 # gcloud cli components
 sudo apt install -y kubectl google-cloud-sdk-skaffold
@@ -29,15 +45,6 @@ curl -k -O https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terrafo
 unzip terraform_${TERRAFORM_VERSION}_linux_amd64.zip
 sudo mv terraform /usr/local/bin/
 rm terraform_${TERRAFORM_VERSION}_linux_amd64.zip
-
-# python
-sudo apt install -y build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev libsqlite3-dev wget libbz2-dev
-wget https://www.python.org/ftp/python/${PYTHON_VERSION}/Python-${PYTHON_VERSION}.tgz
-sudo tar -xvf Python-${PYTHON_VERSION}.tgz
-cd Python-${PYTHON_VERSION}
-sudo ./configure --enable-optimizations
-sudo make -j 4
-sudo make altinstall
 
 # docker
 sudo apt install -y apt-transport-https ca-certificates gnupg lsb-release
